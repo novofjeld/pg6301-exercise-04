6bd2ed90a512786014e77077c3ea7b45bfdcf8d0
camilla novofjeld <cafj001@student.kristiania.no>
2026-09-10 15:33:09 +0200

    make task list whit checkbox

--- src/application.jsx
+++ src/application.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 export function Application() {
+  const [tasks, setTasks] = useState([
+    { description: "task1" },
+    { description: "task2" },
+    { description: "task3" },
+  ]);
+
   return (
     <>
       <h1>My task</h1>
       <ul>
-        <li>First task</li>
+        {tasks.map((t) => (
+          <li>
+            <input type={"checkbox"} /> {t.description}
+          </li>
+        ))}
       </ul>
     </>
   );
