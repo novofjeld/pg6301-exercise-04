c619cc5495966cbf40290f118dab37d266e3aa81
camilla novofjeld <cafj001@student.kristiania.no>
2026-09-10 15:38:05 +0200

    make new task inbox and button

--- src/application.jsx
+++ src/application.jsx
@@ -7,8 +7,24 @@ export function Application() {
     { description: "task3" },
   ]);
 
+  const [description, setDescription] = useState("");
+
   return (
     <>
+      <h2>New task</h2>
+      <form>
+        <div>
+          Task name:
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>Save {description}</button>
+        </div>
+      </form>
       <h1>My task</h1>
       <ul>
         {tasks.map((t) => (
